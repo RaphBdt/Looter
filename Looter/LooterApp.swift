33fa039b7788dc1ca849dfd87a230fe600c58dba
Raphael Beaudet <raphaelbeaudet@icloud.com>
2024-01-10 09:32:18 +0100

    Initial Commit

---
+++ Looter/LooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooterApp.swift
+//  Looter
+//
+//  Created by Raphaël Beaudet on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
