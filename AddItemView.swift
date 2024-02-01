b23fcefa2b2d1516fbf10e3e173dcf33af5f9beb
Raphael Beaudet <raphaelbeaudet@icloud.com>
2024-02-01 23:24:40 +0100

    form to add item

--- AddItemView.swift
+++ AddItemView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 enum Rarity: String, Equatable, CaseIterable {
     case common = "Common"
-    case uncommon = "uncommon"
-    case rare = "rare"
-    case epic = "epic"
-    case legendary = "legendary"
-    case unique = "unique"
+    case uncommon = "Uncommon"
+    case rare = "Rare"
+    case epic = "Epic"
+    case legendary = "Legendary"
+    case unique = "Unique"
     
     var localizedName: LocalizedStringKey { LocalizedStringKey(rawValue) }
 }
@@ -21,6 +21,10 @@ enum Rarity: String, Equatable, CaseIterable {
 struct AddItemView: View {
     @State private var name: String = ""
     @State private var rarity: Rarity = .common
+    @State private var selectedGame: Game? = nil
+    @State private var quantity: Int = 1
+    @State private var selectedType: ItemType = .unknown
+    
     @EnvironmentObject var inventory: Inventory
     @Environment(\.dismiss) private var dismiss
     
@@ -38,8 +42,28 @@ struct AddItemView: View {
                 }
                 
                 Section {
+                    Picker("Jeu", selection: $selectedGame) {
+                        Text("Sélectionnez un jeu").tag(nil as Game?)
+                        ForEach(availableGames, id: \.self) { game in
+                            Text(game.name).tag(game as Game?)
+                        }
+                    }
+                    Stepper("Quantité : \(quantity)", value: $quantity)
+                }
+                
+                Section {
+                    Text("Type")
+                    Picker("Type d'objet", selection: $selectedType) {
+                        ForEach(ItemType.allCases, id: \.self) { type in
+                            Text(type.rawValue)
+                                .tag(type)
+                        }
+                    }.pickerStyle(.palette)
+                }
+                
+                Section {
                     Button("Ajouter") {
-                        let newItem = LootItem(quantity: 1, name: name, type: .unknown, rarity: rarity, attackStrength: nil, game: availableGames[4])
+                        let newItem = LootItem(quantity: quantity, name: name, type: selectedType, rarity: rarity, attackStrength: nil, game: availableGames[4])
                         inventory.addItem(item: newItem)
                         dismiss()
                     }
